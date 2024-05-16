0c272323ec0d0ac017b26c17f248cd057b357a73
Marek Sroczyński <marek123271@gmail.com>
2024-05-16 13:50:42 +0100

    have cart icons

--- sklep/src/Navigation/Nav.js
+++ sklep/src/Navigation/Nav.js
@@ -1,8 +1,32 @@
-import React from 'react'
-import './Nav.css';
-
+import React from "react";
+import "./Nav.css";
+import { FiHeart } from "react-icons/fi";
+import {FiShoppingCart,AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai';
 export default function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your search shoes"
+        />
+      </div>
+
+      <div className="profile-container">
+        <a href ='#'>
+        <FiHeart />
+
+          <FiHeart className="nav-icon" />
+        </a>
+        <a href="/">
+          <AiOutlineShoppingCart className="nav-icons"/>
+        </a>
+        <a href="/">
+          <AiOutlineUserAdd className="nav-icons"/>
+        </a>
+
+      </div>
+    </nav>
+  );
 }
