dc2f404db1691b2e421b5dae3083910681cee7f5
Marek Sroczyński <marek123271@gmail.com>
2024-05-16 15:12:04 +0100

    prodcks done

--- sklep/src/Products/Products.js
+++ sklep/src/Products/Products.js
@@ -1,35 +1,213 @@
-import React, { Component } from 'react'
-import './Products.css';
+import React, { Component } from "react";
+import "./Products.css";
 import { FaStar } from "react-icons/fa";
 import { IoBagAdd } from "react-icons/io5";
-
-
 
 export default class Products extends Component {
   render() {
     return (
       <section className="card-container">
         <section className="card">
-         <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe'/>
-         <div className='card-detalis'>
-          <h3 className='cart-title'>Shoe</h3>
-          <section className='card-reviews'>
-          <FaStar /><FaStar /><FaStar /><FaStar />
-          <span className='total-reviews'>4</span>
-          </section>
-          <section className='card-price'>
-            <div className='price'>
-              <del>300£</del>£200
-            </div>
-            <div className='bag'>
-            <IoBagAdd />
-
-
-            </div>
-          </section>
-         </div>
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="card">
+          <img
+            className="card-image"
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+          />
+          <div className="card-detalis">
+            <h3 className="cart-title">Shoe</h3>
+            <section className="card-reviews">
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <FaStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                <del>300£</del>£200
+              </div>
+              <div className="bag">
+                <IoBagAdd className="bag-icon" />
+              </div>
+            </section>
+          </div>
         </section>
       </section>
-    )
+    );
   }
 }
